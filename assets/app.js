228c24297944b03818d9c1a2b42e8b7b0baff896
Shebby91 <sgrauthoff@gmail.com>
2024-09-15 10:06:32 +0200

    fixed missing font

--- assets/app.js
+++ assets/app.js
@@ -2,9 +2,8 @@
  * This file will be included onto the page via the importmap() Twig function,
  */
 import './bootstrap.js';
-import './lib/singleJavascript.js';
 import singleJavascript from './lib/singleJavascript.js';
 import './styles/app.css';
 
-const alias = new singleJavascript(1, 'Sebastian');
+const alias = new singleJavascript(1, 'sebastian');
 console.log(alias.describe());
