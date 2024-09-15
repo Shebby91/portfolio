c2894bc8b9b1cb87b451efe34bbad1ebf21349a4
Shebby91 <sgrauthoff@gmail.com>
2024-09-15 10:49:02 +0200

    added bootstrap to importmap

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,8 @@
 /*
  * This file will be included onto the page via the importmap() Twig function,
  */
-import './bootstrap.js';
+import './bootstrap.js'; 
 import singleJavascript from './lib/singleJavascript.js';
-import './styles/app.css';
 
 const alias = new singleJavascript(1, 'sebastian');
 console.log(alias.describe());
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.css';
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
