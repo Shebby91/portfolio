5019e3c8c6bde73080324d51dc6c01f3d63601f1
Shebby91 <sgrauthoff@gmail.com>
2024-09-17 16:02:43 +0200

    new fixtures

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -14,6 +14,7 @@ class AuthController extends AbstractController
     {
         return $this->render('security/login.html.twig', [
             'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername(),
             'title' => 'Login',
         ]);
     }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,7 +10,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        UserFactory::createOne(['email'=> 'admin@test.com', 'firstName' => 'Admin', 'password' => 'test']);
+        UserFactory::createOne(['email'=> 'admin@test.com', 'firstName' => 'Admin', 'roles' => ['ROLE_ADMIN']]);
+        UserFactory::createOne(['email'=> 'user@test.com', 'firstName' => 'User']);
         UserFactory::createMany(20);
         $manager->flush();
     }
